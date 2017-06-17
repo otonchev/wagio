75170a496c412e227035817543b29517d4962ff1
Srimanta Panda <srimanta.panda@gmail.com>
2017-06-17 23:03:52 +0200

    Adding bootstrap support

--- src/main/java/com/concretepage/MyApplication.java
+++ src/main/java/com/concretepage/MyApplication.java
@@ -1,9 +1,16 @@
 package com.concretepage;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-@SpringBootApplication
-public class MyApplication {  
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+//import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+
+@ComponentScan
+@EnableAutoConfiguration
+public class MyApplication {
+	
 	public static void main(String[] args) {
 		SpringApplication.run(MyApplication.class, args);
-    }       
+    }
+	
 }            
--- src/main/java/com/concretepage/WebSecurityConfig.java
+++ src/main/java/com/concretepage/WebSecurityConfig.java
@@ -1,17 +1,20 @@
 package com.concretepage;
 
 import javax.sql.DataSource;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.hibernate.SessionFactory;
-import org.hibernate.jpa.HibernateEntityManagerFactory;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.hibernate.SessionFactory;
+import org.hibernate.jpa.HibernateEntityManagerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
@@ -28,7 +31,6 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 	
 	@Autowired
 	public void configAuthentication(AuthenticationManagerBuilder auth) throws Exception {
-		
 		auth.jdbcAuthentication().dataSource(dataSource)
 			.passwordEncoder(new BCryptPasswordEncoder())
 			.usersByUsernameQuery(usersQuery)
@@ -37,8 +39,13 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		http.authorizeRequests().antMatchers("/", "/home").permitAll().anyRequest().authenticated().and().formLogin()
-				.loginPage("/login").permitAll().and().logout().permitAll();
+		http.authorizeRequests().antMatchers("/", "/home", "/index").permitAll().anyRequest().authenticated().and()
+				.formLogin().loginPage("/login").permitAll().and().logout().permitAll();
+	}
+
+	@Override
+	public void configure(WebSecurity web) throws Exception {
+		web.ignoring().antMatchers("/the_js_path/**");
 	}
 	
 	@Bean  
