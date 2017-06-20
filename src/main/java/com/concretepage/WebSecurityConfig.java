ce2cb4b86c282b0bd38870ca3fdac95e575c8b12
Ognyan Tonchev <ognyan@axis.com>
2017-06-20 15:39:40 +0200

    put back @EnableWebSecurity and excluded /webjars/** from protected paths

--- src/main/java/com/concretepage/WebSecurityConfig.java
+++ src/main/java/com/concretepage/WebSecurityConfig.java
@@ -11,10 +11,12 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Configuration
+@EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
 	@Autowired
@@ -36,7 +38,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		http.authorizeRequests().antMatchers("/", "/home", "/index").permitAll().anyRequest().authenticated().and()
+		http.authorizeRequests().antMatchers("/", "/home", "/index", "/webjars/**").permitAll().anyRequest().authenticated().and()
 				.formLogin().loginPage("/login").permitAll().and().logout().permitAll();
 	}
 
